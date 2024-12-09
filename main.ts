da5d22e65177c150a607b83ebda981d535651b63
antebrinken@live.se <antebrinken@live.se>
2024-12-09 14:15:14 +0100

    changesp

--- main.ts
+++ main.ts
@@ -1,1 +1,27 @@
 import './style.css'
+
+window.onload = function() {
+
+const myName: string = "Philip"
+
+let output = ""; 
+
+for (let i = 0; i < myName.length; i++) {
+  output += `<p>${myName[i]}</p>`;
+
+      
+      if (i < myName.length - 1) {
+        output += " ";
+
+        console.log(myName);
+        
+    }
+}
+
+
+
+
+
+}
+
+
