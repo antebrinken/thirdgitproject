24f0d93eda6fbc0b6a617220256476e2510aaf0f
AmandaBalk <Amanda_Balck@hotmail.com>
2024-12-09 14:16:58 +0100

    added code

--- main.ts
+++ main.ts
@@ -1,1 +1,11 @@
 import './style.css'
+
+const myName = ()=> {
+  const name = "Amanda"
+  console.log(name.length)
+
+  for (let i = 0; i < name.length; i++){
+    console.log(name[i]);
+  }
+}
+myName();
